c2316196dfb938dbaca108bfa4ea9692228e8e53
junaid989 <muhjunaid989@gmail.com>
2025-11-13 20:55:25 +0530

    Refactor gradlew.bat with comments and formatting
    Updated the gradlew.bat script for improved readability and added comments.

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,6 @@
-@ECHO OFF
-SET DIR=%~dp0
+@echo off
+REM ---------------------------------------------------------------------------
+REM Gradle startup script for Windows
+REM ---------------------------------------------------------------------------
+set DIR=%~dp0
 java -jar "%DIR%gradle\wrapper\gradle-wrapper.jar" %*
